859539fd3788485aa261460d42be976d4d41cc80
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-03-25 00:03:41 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :authors, only: [:show] do
-  
+
   resources :posts, only: [:show, :index]
-  
+
   root 'posts#index'
-  
+
 
 end
